e7e7e0c87e7e1aff3d94b91dce472e1e4dd811d4
gaebaljoah <ljm5436124@naver.com>
2023-10-26 17:21:01 +0900

    웹 mvc 개발 회원관리 등록 조회 간단예제

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -8,6 +8,9 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+		//우선순위 컨테이너 안 스프링 빈에 등록되어있는 컨트롤러부터 매핑한 후 없으면 그때 static 폴더에있는 파일을 찾음.
+		//@getmapping("/")이 homecontroller에 존재하므로 home.html 파일로 이동했다.
+
 	}
 
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //어노테이션 선언시 스프링 컨테이너에서 관리 컨테이너에서 관리하니 스프링관련된 기능들이 작동하는것이다.
 public class MemberController {
@@ -16,4 +22,30 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "/members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
+
+
 }
