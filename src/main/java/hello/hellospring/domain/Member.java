562ebd1aa3c20ddb0dd6cb136f02df6a169e99f2
gaebaljoah <ljm5436124@naver.com>
2023-10-31 11:11:48 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity //jpa가 관리하는 entity라고 표현
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //pk를 mapping @GeneratedValue(strategy = GenerationType.IDENTITY) = sequence 개념
     private Long id;
+
+    @Column(name="username") //컬럼을 매핑
     private String name;
 
     public Long getId() {
